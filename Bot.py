fa95567aa1b09247e2e0c02c1c6835ecb3956235
Qui-Gon-Jin <plokun2000@gmail.com>
2020-07-08 21:08:23 +0300

    !uptime added

--- Bot.py
+++ Bot.py
@@ -1,4 +1,5 @@
 import discord
+from datetime import datetime
 from discord.ext import commands
 import Help
 import Roll
@@ -8,10 +9,13 @@ from Character import Character
 client = discord.Client()
 roll_result = []
 characters = dict()
+
+startup_time = datetime.now()
+
 @client.event
 async def on_ready():
 	print('We have logged in as {0.user}'.format(client))
-
+	print(str(startup_time))
 @client.event
 async def on_message(message):
 	global characters
@@ -23,6 +27,13 @@ async def on_message(message):
 		await message.channel.send(Help.help())
 	if message.content.startswith('!set'):
 		await message.channel.send(Settings.show_settings())
+	if message.content.startswith('!uptime'):
+		current_time = datetime.now()
+		diff_time = current_time - startup_time
+		startup_time = str(startup_time).split('.')[0]
+		diff_time = str(diff_time).split('.')[0]
+ 
+		await message.channel.send('```Started at: \t' + startup_time + '\nUptime: \t\t' + diff_time + '```')
 
 	if message.content.startswith('!roll'):
 		roll = message.content.split(" ")
@@ -32,6 +43,10 @@ async def on_message(message):
 		await message.channel.send(output)
 
 	if message.content.startswith('!add_char'):
+		character = message.content.split(" ")
+		characters[author] = Character(character, author)
+		
+	if message.content.startswith('!mod_char'):
 		character = message.content.split(" ")
 		characters[author] = Character(character, author)
 
