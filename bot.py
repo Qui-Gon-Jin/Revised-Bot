d3b806df6674e17eb19a0ad301cfacf9a2112746
Qui-Gon-Jin <plokun2000@gmail.com>
2021-02-22 10:05:15 +0200

    Rolling rezult calculating

--- bot.py
+++ bot.py
@@ -5,8 +5,6 @@ import help_call
 import roll
 
 client = discord.Client()
-characters = dict()
-
 startup_time = datetime.now()
 
 @client.event
@@ -22,18 +20,9 @@ async def on_message(message):
 	if message.content.startswith('!help'):
 		await message.channel.send(help_call.help())
 
-	if message.content.startswith('!uptime'):
-		current_time = datetime.now()
-		diff_time = current_time - startup_time
-		startup_time = str(startup_time).split('.')[0]
-		diff_time = str(diff_time).split('.')[0]
-		await message.channel.send('```Started at: \t' + startup_time + '\nUptime: \t\t' + diff_time + '```')
-
 	if message.content.startswith('!roll') or message.content.startswith('!r'):
-		#output = roll.rolling_function(str(message.content), str(message.author))
 		roll_rezult = roll.roll(str(message.content), str(message.author))
-		output = roll_rezult.rolling()
-		await message.channel.send(output)
+		await message.channel.send(roll_rezult.rolling())
 
 token = open('test_token.txt', 'r')
 client.run(token.read())
--- roll.py
+++ roll.py
@@ -4,7 +4,6 @@ import settings
 
 class roll:
 	def __init__(self, message, author):
-		self.message = message
 		self.author = author
 		self.crit_fail_check = True
 		self.difficulty = 6
@@ -12,25 +11,47 @@ class roll:
 		self.roll_result = []
 		self.params = []
 		self.roll = message.split(" ")
+		self.result = [0, ""]
 
 
 	def rolling(self):
 		del self.roll[0]
 
-		if "-" in self.roll[-1]:
+		if "-" in self.roll[-1]:	#check for parametrs
 			self.params.append(self.roll[-1][1:])
 			del self.roll[-1]
 
-		if len(self.roll) > 1:
-			self.difficulty = self.roll[-1]
+		if len(self.roll) > 1:		#cut the difficulty
+			self.difficulty = int(self.roll[-1])
 			del self.roll[-1]
 
-		if "d" in self.roll[0]:
+		if "d" in self.roll[0]:		#count dices
 			self.dice[0] = int(self.roll[0].split("d")[0])
 			self.dice[1] = int(self.roll[0].split("d")[1])
 		else:
 			self.dice[0] = int(self.roll[0].split("d")[0])
 
+		i = 0
+		while i < self.dice[0]:		#count throw
+			self.roll_result.append(random.randint(1, int(self.dice[1])))
+			i += 1
+			if self.roll_result[-1] == self.dice[1] and "e" in self.params: #explosion
+				i -= 1
+
+		for i in self.roll_result:
+			if i >= self.difficulty:
+				self.result[0] += 1
+			if i == 1:
+				self.result[0] -= 1
+
+		if self.result[0] > 0:
+			self.result[1] = "success"
+		elif self.result[0] <= 0:
+			self.result[1] = "fail"
 
 
-		return("```dices: " + str(self.dice) + "\ndiff: " + str(self.difficulty) + "\nparams: " + str(self.params) + "```")
+
+		return("```dices: " + str(self.dice) + "\ndiff: " +
+			str(self.difficulty) + "\nparams: " + str(self.params) +
+			"\nroll array: " + str(self.roll_result) + "\nresult" + str(self.result) + 
+			"```")
