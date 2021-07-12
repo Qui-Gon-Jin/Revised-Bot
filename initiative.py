69ba629226cd403a93363dc8e7aec3ef73ebfbed
vladyslav.bohdanov <vladyslav.bohdanov@visiquate.com>
2021-07-12 17:49:39 +0300

    Added initiative value to output

--- initiative.py
+++ initiative.py
@@ -10,13 +10,11 @@ class initiative:
 
     def init(self):
         del self.roll[0]
-        self.roll.append(random.randint(1, 10))
-        if len(self.roll) == 2:
-            self.result = int(self.roll[0]) + int(self.roll[0]) + int(self.roll[1])
-            output = (self.author + "\nInitiative: " + str(self.result) + "\nValues: [" + str(self. roll[0]) + ", " + str(self. roll[0]) + ", " + str(self. roll[1]) + "]")
+        if len(self.roll) == 1:
+            self.roll.append(self.roll[0])
 
-        if len(self.roll) == 3:
-            self.result = int(self.roll[0]) + int(self.roll[1]) + int(self.roll[2])
-            output = (self.author + "\nInitiative: " + str(self.result) + "\nValues: [" + str(self. roll[0]) + ", " + str(self. roll[1]) + ", " + str((self. roll[2])) + "]")
-        
+        self.roll.append(random.randint(1, 10))
+            
+        self.result = int(self.roll[0]) + int(self.roll[1]) + int(self.roll[2])
+        output = (self.author + "\nInitiative: " + str(self.result) + "\nInitiative rating: " + str(int(self. roll[0]) + int(self. roll[1])) + "\nValues: [" + str(self. roll[0]) + ", " + str(self. roll[1]) + ", " + str((self. roll[2])) + "]")
         return("```" + output + "```")
